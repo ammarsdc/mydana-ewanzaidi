a8f4892980b14113e8a2a5b06de30cfca2168515
Ewan Zaidi <ridhwanzaidi@gmail.com>
2018-02-04 11:44:48 +0800

    implement all

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
+
+var CONTACTS_COLLECTION = "contacts";
+
+var app = express();
+app.use(bodyParser.json());
+
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
